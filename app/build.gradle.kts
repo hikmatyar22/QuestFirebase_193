1147c4f9bdad30d67005201b8dcf5aea3b6cc0bd
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2026-01-02 19:44:46 +0700

    Update Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    implementation(libs.compose.icons)
+
     //Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
@@ -63,6 +65,7 @@ dependencies {
     // viewmodel
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.androidx.material3)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
