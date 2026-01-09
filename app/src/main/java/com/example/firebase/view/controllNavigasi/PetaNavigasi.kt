3b1d7840995c78da631eb7cecd6a9a2bd5ba77cf
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2026-01-09 17:42:20 +0700

    menambahkan destinasti edit siswa di navigasi yang digunakan untuk mengatur navigasi ke halaman edit siswa dengan membawa ID sebagai argumen, serta menyediakan aksi untuk kembali ke halaman utama atau kembali ke layar sebelumnya.

--- app/src/main/java/com/example/firebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/firebase/view/controllNavigasi/PetaNavigasi.kt
@@ -75,7 +75,23 @@ fun HostNavigasi(
             )
         }
 
-
+        composable(
+            DestinasiEdit.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiEdit.itemIdArg) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            EditSiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                },
+                onNavigateUp = {
+                    navController.navigateUp()
+                }
+            )
+        }
     }
 }
 
